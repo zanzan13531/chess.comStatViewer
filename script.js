d5d763e05039e958cb17ab3f234bb31ae64fef33
zanzan13531 <zanzan13531@gmail.com>
2021-02-08 21:22:16 -0800

    stuff from punch

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const form = document.querySelector("#my-form");
+
+form.addEventListener("submit", (event)=>{
+
+    event.preventDefault();
+
+    const data = form.elements;
+
+    testyText.innerText = data.username.value;
+    
+    //console.log(data.username.value);
+    //console.log(data.remember.value);
+
+});
+
+
 function doSearchStuff() {
 
     var form = document.getElementById("searchbar");
