b5b715edd63dc769df42b02c4e114516faabd8bb
zanzan13531 <zanzan13531@gmail.com>
2021-02-09 21:18:00 -0800

    added bunch of div s to form basic structure, some hiding of elements added in js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+hideAll();
+
 const form = document.querySelector("#searchbar");
 
 form.addEventListener("submit", (event)=>{
@@ -6,11 +8,45 @@ form.addEventListener("submit", (event)=>{
 
     const data = form.elements;
 
-    testyText.innerText = data.username.value;
-    
+    usernameText.innerText = data.username.value;
+
+    if (data.username.value == "no user") {
+
+        showUserNotFound();
+
+    } else {
+
+        showResults();
+
+    }
     //console.log(data.username.value);
     //console.log(data.remember.value);
 
     return false;
 
 });
+
+function hideAll() { //searchbarPart is always visible
+
+    userNotFound.hidden = true;
+    userInfo.hidden = true;
+    timeControlRatingInfo.hidden = true;
+    openingInfo.hidden = true;
+
+}
+
+function showUserNotFound() {
+
+    hideAll();
+    userNotFound.hidden = false;
+
+}
+
+function showResults() {
+    
+    hideAll();
+    userInfo.hidden = false;
+    timeControlRatingInfo.hidden = false;
+    openingInfo.hidden = false;
+
+}
